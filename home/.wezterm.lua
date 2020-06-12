888ccb4501305091e9a925cf6e1e309dba558c04
Rain <rain1@fb.com>
2020-06-12 11:22:25 -0700

    [wezterm] don't send composed keys when alt is pressed.
    Composed keys break alt-based navigation (alt-b, alt-f) on my setup.

--- home/.wezterm.lua
+++ home/.wezterm.lua
@@ -4,5 +4,6 @@ return {
   color_scheme = "Ubuntu",
   font_size = 10.0,
   harfbuzz_features = {"ss01", "ss02", "ss03", "ss04", "ss06", "zero", "onum"},
+  send_composed_key_when_alt_is_pressed = false,
   scrollback_lines = 5000,
 }
