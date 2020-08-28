b57e42d26630bb6598e7070d73bdbf0c067195ba
Rain <rain@sunshowers.io>
2020-08-28 14:41:36 -0700

    update a bunch of settings

--- home/.wezterm.lua
+++ home/.wezterm.lua
@@ -3,6 +3,8 @@ return {
   font = wezterm.font("Fira Code"),
   color_scheme = "Ubuntu",
   font_size = 10.0,
+  font_antialias = "Subpixel",
+  font_hinting = "Full",
   harfbuzz_features = {"ss01", "ss02", "ss03", "ss04", "ss06", "zero", "onum"},
   send_composed_key_when_alt_is_pressed = false,
   scrollback_lines = 5000,
